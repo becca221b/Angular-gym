f8d9c8ef6549e1f2feb417f9fb99cacf17e33e11
Rebeca Ozuna <rebecao@academica.local>
2023-12-28 15:30:38 -0300

    main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Client } from './models/client'; 
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,29 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'miProyecto';
+  
+  clienteArray: Client[]= [];
+
+  selectedClient: Client = new Client();
+
+  addOrEdit(){
+    if(this.selectedClient.id===0){
+      this.selectedClient.id = this.clienteArray.length+1;
+      this.clienteArray.push(this.selectedClient);
+    }
+    
+    this.selectedClient = new Client()
+  }
+
+  openForEdit(client: Client){
+    this.selectedClient= client
+  }
+  
+  delete(){
+    if(confirm(`Estás seguro de querer eliminar a ${this.selectedClient.name}`)){
+      this.clienteArray = this.clienteArray.filter( x=> x != this.selectedClient);
+      this.selectedClient = new Client();
+    }
+  }
+  
 }
