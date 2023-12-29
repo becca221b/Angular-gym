53397ce64595723ba0ff52f967d442fd65438409
Rebeca Ozuna <rebecao@academica.local>
2023-12-29 20:51:07 -0300

    change form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { Client } from './models/client'; 
+import { Client } from './models/client';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'miProyecto';
+  title = 'Sparta Gym';
   
   clienteArray: Client[]= [];
 
